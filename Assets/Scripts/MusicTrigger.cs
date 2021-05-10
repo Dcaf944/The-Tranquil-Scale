65bf0ffdeda9a7d8010c9c174cd04ec235091993
Dcaf944 <thomas100@live.com.au>
2021-05-10 14:38:23 +1000

    New
    Update on new animations

--- Assets/Scripts/MusicTrigger.cs
+++ Assets/Scripts/MusicTrigger.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class MusicTrigger : MonoBehaviour
 {
     public GameObject objectToSpawn;
-
     public AudioClip pianoKeySFX;
     public GameObject particleEffect;
     public Transform particleEffectLocation;
     AudioSource audio;
     public float Volume;
 
+    public GameObject nextKeyToPLay;
+    public GameObject keyToHide;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         objectToSpawn.SetActive(false);
-        audio = GetComponent<AudioSource>();  
+        audio = GetComponent<AudioSource>();
+        nextKeyToPLay.SetActive(false);
+        keyToHide.SetActive(true);
     }
 
     // Update is called once per frame
@@ -28,14 +33,25 @@ public class MusicTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+
         if (other.CompareTag("Hand"))
         {
+            Collider col = GetComponent<Collider>();
+            col.enabled = false;
             objectToSpawn.SetActive(true);
             Instantiate(particleEffect, particleEffectLocation.position, particleEffectLocation.rotation);
             audio.PlayOneShot(pianoKeySFX, Volume);
             GetComponent<Animator>().Play("KeyPress");
+            Invoke("NextKey", 15);
         }
 
     }
+
+    void NextKey()
+    {
+        nextKeyToPLay.SetActive(true);
+        keyToHide.SetActive(false);
+    }
+
 
 }
