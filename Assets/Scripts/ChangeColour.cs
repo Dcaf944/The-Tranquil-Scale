4025e0e62306598d1c9cba9d2d7ae629cab25226
Dcaf944 <thomas100@live.com.au>
2021-05-17 03:30:49 +1000

    Animations music and Colour change
    More keys working with more animations and colour change Testing

---
+++ Assets/Scripts/ChangeColour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColour : MonoBehaviour
+{
+
+    public float speed = 1.0f;
+    public Color startColour;
+    public Color endColour;
+    float startTime;
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = (Time.time - startTime) * speed;
+        GetComponent<Renderer>().material.color = Color.Lerp(startColour, endColour, t);
+    }
+}
