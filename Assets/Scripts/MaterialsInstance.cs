6cda1426b887bfd96337719d41c26aed5f12add1
Dcaf944 <thomas100@live.com.au>
2021-05-24 11:04:30 +1000

    New material optimization
    Optimization

---
+++ Assets/Scripts/MaterialsInstance.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialsInstance : MonoBehaviour
+{
+
+    public GameObject mObject;
+    public Color colour;
+    public Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mObject = this.gameObject;
+        material = mObject.GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        material.color = colour;
+    }
+}
