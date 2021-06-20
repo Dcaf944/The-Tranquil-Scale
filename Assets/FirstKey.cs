1d8c7c003d710c7a5d11e2111fd1d591a7d0b212
Dcaf944 <thomas100@live.com.au>
2021-06-21 02:49:34 +1000

    Final Update
    Everything is complete

---
+++ Assets/FirstKey.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstKey : MonoBehaviour
+{
+
+    public GameObject startingText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingText.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        startingText.SetActive(false);
+    }
+
+
+}
